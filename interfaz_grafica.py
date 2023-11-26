e9f38f7b4d7b06341ba5b1e10d308160a581577f
MartinFerreyra01 <martinferreyra01@gmail.com>
2023-11-26 19:23:38 -0300

    Objetivo 1
    Cambios en casi todas las funciones de interfaz_grafica.py, reorganización de archivos, todavía trabajando en los intentos de recuperación

--- interfaz_grafica.py
+++ interfaz_grafica.py
@@ -5,32 +5,7 @@ from cifrado_atbash import cifrado_atbash
 from validacion_de_datos import validar_registro_usuario
 import csv
 
-# Función para agregar preguntas
-def agregar_preguntas():
-    nuevas_preguntas = [
-        (6, "Color favorito"),
-        (7, "Comida preferida"),
-        (8, "Lugar de nacimiento"),
-        (9, "Libro favorito"),
-        (10, "Hobby principal"),
-    ]
 
-    # Leer preguntas existentes del archivo
-    preguntas_existentes = []
-    with open('preguntas.csv', 'r', newline='', encoding='utf-8') as archivo_csv:
-        lector_csv = csv.reader(archivo_csv)
-        preguntas_existentes = list(lector_csv)
-
-    # Agregar nuevas preguntas
-    for nueva_pregunta in nuevas_preguntas:
-        preguntas_existentes.append(nueva_pregunta)
-
-    # Guardar preguntas actualizadas en el archivo
-    with open('preguntas.csv', 'w', newline='', encoding='utf-8') as archivo_csv:
-        escritor_csv = csv.writer(archivo_csv)
-        escritor_csv.writerows(preguntas_existentes)
-
-agregar_preguntas()
 # Función para la ventana de bienvenida
 def mostrar_ventana_bienvenida():
     '''Abre la primer ventana para interactuar con el usuario. Contiene boton para continuar a la segunda ventana.
@@ -38,17 +13,17 @@ def mostrar_ventana_bienvenida():
     '''
     ventana_bienvenida = Tk()
     ventana_bienvenida.title("TP Grupal Parte 1- Grupo [Codeo]")
-    ventana_bienvenida.iconbitmap("icono.ico")
+    ventana_bienvenida.iconbitmap("./logos/icono.ico")
     ventana_bienvenida.resizable(0, 0)
     ventana_bienvenida.geometry("350x300")
     ventana_bienvenida.config(bg="#87CEFA")
-    ventana_bienvenida.rowconfigure(0, weight=75)
+    ventana_bienvenida.rowconfigure(0, weight=50)
     ventana_bienvenida.rowconfigure(3, weight=100)
     ventana_bienvenida.rowconfigure(6, weight=100)
     ventana_bienvenida.columnconfigure(0, weight=1)
     ventana_bienvenida.columnconfigure(3, weight=1)
 
-    Label(ventana_bienvenida, text="Bienvenido a la aplicación\nde mensajes secretos del grupo [Codeo]\n Para continuar presione continuar, \nde lo contrario cierre la ventana").grid(row=1, column=1, columnspan=2, pady=10, sticky=W+E, ipady=5, ipadx=10)
+    Label(ventana_bienvenida, text="Bienvenido a la aplicación\nde mensajes secretos del grupo [Codeo]\n Para continuar presione continuar, \nde lo contrario cierre la ventana").grid(row=1, column=1, columnspan=2, pady=15, sticky=W+E, ipady=5, ipadx=10)
     Button(ventana_bienvenida, text="Crear Usuario", command=mostrar_ventana_registro).grid(row=2, column=1, columnspan=2, sticky=W+E)
     Button(ventana_bienvenida, text="Ingreso Usuario", command=mostrar_ventana_ingreso).grid(row=3, column=1, columnspan=2, sticky=W+E)
     Label(ventana_bienvenida, text="Construída por", fg="#8c8c8c").grid(row=4, column=1, columnspan=2, pady=10, sticky=W+E+S, ipady=5)
@@ -57,141 +32,167 @@ def mostrar_ventana_bienvenida():
     ventana_bienvenida.mainloop()
 
 
-# Variables globales para almacenar datos de registro
-usuario_var = None
-clave_var = None
-pregunta_var = None
-respuesta_var = None
-ventana_registro = None
-
-
-# Función para la ventana de registro
 def mostrar_ventana_registro():
-    def validar_campos():
-        global usuario_var, clave_var, pregunta_var, respuesta_var
 
-        usuario_var = entry_usuario.get()
-        clave_var = entry_clave.get()
-        pregunta_var = combo_preguntas.get()
-        respuesta_var = entry_respuesta.get()
-
-        if not usuario_var or not clave_var or pregunta_var == "Seleccione una pregunta" or not respuesta_var:
-            messagebox.showerror("Error", "Por favor, complete todos los campos.")
-            return False
-
-        return True
-
-    def validar_registro():
-        if validar_campos():
-            
-            validar_registro_usuario(usuario_var, clave_var, pregunta_var, respuesta_var)
-
-    global ventana_registro
-    ventana_registro = Tk()
-    ventana_registro.title("TP Grupal Parte 1- Grupo [Codeo]")
-    ventana_registro.iconbitmap("icono.ico")
+    # Configuracion ventana
+    ventana_registro = Toplevel()
+    ventana_registro.title("Registro de usuario")
+    ventana_registro.iconbitmap("./logos/icono.ico")
     ventana_registro.resizable(0, 0)
     ventana_registro.geometry("350x300")
     ventana_registro.config(bg="#87CEFA")
-    ventana_registro.rowconfigure(0, weight=75)
-    ventana_registro.rowconfigure(3, weight=100)
-    ventana_registro.rowconfigure(6, weight=100)
+    ventana_registro.rowconfigure(0, weight=1)
+    ventana_registro.rowconfigure(4, weight=1)
+    ventana_registro.rowconfigure(8, weight=1)
     ventana_registro.columnconfigure(0, weight=1)
-    ventana_registro.columnconfigure(3, weight=1)
+    ventana_registro.columnconfigure(3, weight=1)  
 
-    Label(ventana_registro, text="Ingrese un usuario:").grid(row=1, column=1, sticky=W+E, pady=5, ipadx=10, ipady=3)
+
+    # Labels y botones
+    Label(ventana_registro, text="Ingrese un usuario:").grid(row=1, column=1, sticky=W+E, pady=5, padx=5)
+    Label(ventana_registro, text="Ingrese una clave").grid(row=2, column=1, sticky=W+E, pady=5, padx=5)
+
     entry_usuario = Entry(ventana_registro)
-    entry_usuario.grid(row=1, column=2, sticky=E, padx=5)
+    entry_usuario.grid(row=1, column=2)
 
-    Label(ventana_registro, text="Ingrese una clave:").grid(row=2, column=1, sticky=W+E, pady=5, ipadx=10, ipady=3)
-    entry_clave = Entry(ventana_registro, show="*")
-    entry_clave.grid(row=2, column=2, sticky=E, padx=5)
+    entry_clave = Entry(ventana_registro)
+    entry_clave.grid(row=2, column=2)
 
-    preguntas = [
-        "Apellido de su abuela materna",
-        "Nombre de tu mascota",
-        "Nombre de tu mejor amigo/amiga",
-        "Cantante preferido",
-        "Ciudad preferida"
-    ]
-    combo_preguntas = ttk.Combobox(ventana_registro, values=preguntas)
-    combo_preguntas.set("Seleccione una pregunta")
-    combo_preguntas.grid(row=3, column=1, sticky=E, padx=15, pady=5)
+    # Menu desplegable
+    with open("./archivos csv/preguntas.csv") as file:
+        preguntas = []
+        reader = csv.reader(file)
+        for row in reader:
+            preguntas.append(row[1])
 
-    Label(ventana_registro, text="Ingrese su respuesta:").grid(row=4, column=1, sticky=W+E, pady=5, ipadx=10, ipady=3)
-    entry_respuesta = Entry(ventana_registro, show="*")
-    entry_respuesta.grid(row=4, column=2, sticky=E, padx=5)
+    desplegable_preguntas = ttk.Combobox(ventana_registro, values=preguntas)
+    desplegable_preguntas.set("Seleccione pregunta")
+    desplegable_preguntas.grid(row=3, column=1, sticky=W+E, pady=5, padx=5)
 
-    Button(ventana_registro, text="Crear Usuario", command=validar_registro).grid(row=5, column=1, columnspan=2, sticky=W+E)
+    entry_respuesta_pregunta = Entry(ventana_registro)
+    entry_respuesta_pregunta.grid(row=3, column=2, sticky=W+E, pady=10, padx=5)
+
+    # Boton para registrar
+    Button(ventana_registro, text="Registrar",command= lambda: validar_registro_usuario(entry_usuario.get(), entry_clave.get(), desplegable_preguntas.get(), entry_respuesta_pregunta.get())).grid(row=5, column=1, columnspan=2, sticky=W+E, pady=10)
+
+    # Requisitos
+    Label(ventana_registro, text="El identificador debe estar compuesto solo de\nletras, numeros o los caracteres '_' '-' y '.'\ny debe tener minimo 5 y maximo 15 caracteres", justify="left", fg="#8c8c8c").grid(row=6, column=1, columnspan=2, sticky=W+E, pady=5)
+    Label(ventana_registro, text="Requisitos de la clave:\n- De 4 a 8 caracteres\t- Al menos un numero\n- Al menos una mayuscula\t- Incluye '_' '-' '#' o '*'\n- Al menos una minuscula\t- Adyacentes no repetidos", justify="left", fg="#8c8c8c").grid(row=7, column=1, columnspan=2)
+
 
 def mostrar_ventana_ingreso():
+
+    # Configuracion ventana
     ventana_ingreso = Toplevel()
     ventana_ingreso.title("Identificación para acceso")
-    ventana_ingreso.geometry("350x300")
+    ventana_ingreso.iconbitmap("./logos/icono.ico")
+    ventana_ingreso.resizable(0, 0)
+    ventana_ingreso.geometry("350x200")
     ventana_ingreso.config(bg="#87CEFA")
-    ventana_ingreso.rowconfigure(0, weight=75)
-    ventana_ingreso.rowconfigure(3, weight=100)
-    ventana_ingreso.rowconfigure(6, weight=100)
+    ventana_ingreso.rowconfigure(0, weight=1)
+    ventana_ingreso.rowconfigure(3, weight=1)
+    ventana_ingreso.rowconfigure(6, weight=1)
     ventana_ingreso.columnconfigure(0, weight=1)
     ventana_ingreso.columnconfigure(3, weight=1)
 
-    Label(ventana_ingreso, text="Ingrese su usuario:").grid(row=0, column=0, sticky=W+E, pady=5, ipadx=10, ipady=3)
+
+    # Labels y entrys
+    Label(ventana_ingreso, text="Ingrese su usuario:").grid(row=1, column=1, sticky=W+E, pady=5, ipadx=10, ipady=1)
     entry_usuario_ingreso = Entry(ventana_ingreso)
-    entry_usuario_ingreso.grid(row=0, column=1, sticky=E, padx=5)
+    entry_usuario_ingreso.grid(row=1, column=2, sticky=E, padx=5)
 
-    Label(ventana_ingreso, text="Ingrese su clave:").grid(row=1, column=0, sticky=W+E, pady=5, ipadx=10, ipady=3)
+    Label(ventana_ingreso, text="Ingrese su clave:").grid(row=2, column=1, sticky=W+E, pady=5, ipadx=10, ipady=1)
     entry_clave_ingreso = Entry(ventana_ingreso, show="*")
-    entry_clave_ingreso.grid(row=1, column=1, sticky=E, padx=5)
+    entry_clave_ingreso.grid(row=2, column=2, sticky=E, padx=5)
 
-    Button(ventana_ingreso, text="Ingresar", command=lambda: validar_ingreso(ventana_ingreso, entry_usuario_ingreso, entry_clave_ingreso)).grid(row=2, column=1, columnspan=2, sticky=W+E)
-    Button(ventana_ingreso, text="He olvidado mi contraseña", command= recuperacion_clave).grid(row=3, column=1, columnspan=2, sticky=W+E)
+    Button(ventana_ingreso, text="Ingresar", command=lambda: validar_ingreso(ventana_ingreso, entry_usuario_ingreso.get(), entry_clave_ingreso.get())).grid(row=4, column=1, columnspan=2, sticky=W+E)
+    Button(ventana_ingreso, text="He olvidado mi contraseña", command= ventana_recuperar_clave).grid(row=5, column=1, columnspan=2, sticky=W+E, pady=5)
+    
     # Vincula la tecla Enter al botón
-    entry_clave_ingreso.bind("<Return>", lambda event=None: validar_ingreso(ventana_ingreso, entry_usuario_ingreso, entry_clave_ingreso))
+    entry_clave_ingreso.bind("<Return>", lambda event=None: validar_ingreso(ventana_ingreso, entry_usuario_ingreso.get(), entry_clave_ingreso.get()))
 
-def recuperacion_clave():
-    ventana_recuperacion= Toplevel()
-    ventana_recuperacion.title("recuperación clave")
-    ventana_recuperacion.geometry("350x300")
+
+def ventana_recuperar_clave():
+
+    # Configuracion ventana
+    ventana_recuperacion = Toplevel()
+    ventana_recuperacion.title("Recuperación clave")
+    ventana_recuperacion.iconbitmap("./logos/icono.ico")
+    ventana_recuperacion.resizable(0, 0)
+    ventana_recuperacion.geometry("350x250")
     ventana_recuperacion.config(bg="#87CEFA")
-    ventana_recuperacion.rowconfigure(0, weight=75)
-    ventana_recuperacion.rowconfigure(3, weight=100)
-    ventana_recuperacion.rowconfigure(6, weight=100)
+    ventana_recuperacion.rowconfigure(0, weight=1)
+    ventana_recuperacion.rowconfigure(4, weight=1)
+    ventana_recuperacion.rowconfigure(6, weight=1)
     ventana_recuperacion.columnconfigure(0, weight=1)
+    ventana_recuperacion.columnconfigure(1, weight=1)
+    ventana_recuperacion.columnconfigure(2, weight=1)
     ventana_recuperacion.columnconfigure(3, weight=1)
 
-    Label(ventana_recuperacion, text="Si ha olvidado su contraseña,por favor complete todos los campos.").grid(row=0, column=0, sticky=W+E, pady=5, ipadx=10, ipady=3)
-    Label(ventana_recuperacion, text="Ingrese su usuario:").grid(row=1, column=0, sticky=W+E, pady=5, ipadx=10, ipady=3)
+    # Labels y entrys
+    Label(ventana_recuperacion, text="Si ha olvidado su contraseña,\npor favor complete todos los campos.").grid(row=1, column=1, columnspan=2, sticky=W+E, pady=5, ipady=5)
+    Label(ventana_recuperacion, text="Ingrese su usuario:").grid(row=2, column=1, sticky=W+E, pady=5, ipady=1)
+    
     entry_usuario_recuperacion = Entry(ventana_recuperacion)
-    entry_usuario_recuperacion.grid(row=1, column=1, sticky=E, padx=5)
+    entry_usuario_recuperacion.grid(row=2, column=2, sticky=W+E, padx=5)
 
     
-def validar_ingreso(ventana_ingreso, entry_usuario_ingreso, entry_clave_ingreso):
-    id_usuario_ingreso = entry_usuario_ingreso.get()
-    clave_ingreso = entry_clave_ingreso.get()
-
-    with open('usuarios.csv', 'r') as file:
+    # Desplegable de preguntas
+    with open("./archivos csv/preguntas.csv") as file:
+        preguntas = []
         reader = csv.reader(file)
         for row in reader:
-            if row and row[0] == id_usuario_ingreso and row[1] == clave_ingreso:
+            preguntas.append(row[1])
+
+    desplegable_preguntas = ttk.Combobox(ventana_recuperacion, values=preguntas)
+    desplegable_preguntas.set("Seleccione pregunta")
+    desplegable_preguntas.grid(row=3, column=1, sticky=W+E, pady=5, padx=5)
+
+    entry_respuesta_pregunta = Entry(ventana_recuperacion)
+    entry_respuesta_pregunta.grid(row=3, column=2, sticky=W+E, pady=10, padx=5)
+
+    Button(ventana_recuperacion, text="Recuperar", command= lambda: recuperar_clave(entry_usuario_recuperacion.get(), desplegable_preguntas.get(), entry_respuesta_pregunta.get())).grid(row=5, column=1, columnspan=2, sticky=W+E)
+
+
+def recuperar_clave(id_usuario, id_pregunta, respuesta_recuperacion):
+    entradas_validas = False
+
+    with open('./archivos csv/usuarios.csv', 'r+') as file:
+        reader = csv.reader(file)
+        writer = csv.writer(file)
+        for row in reader:
+            if row and row[0] == id_usuario and row[2] == id_pregunta and row[3] == respuesta_recuperacion:
+                messagebox.showinfo("Éxito", f"Su contraseña es: {row[1]}")
+                entradas_validas = True
+            elif row and row[0] == id_usuario and (row[2] != id_pregunta or row[3] != respuesta_recuperacion):
+                messagebox.showerror("Error", "Respuesta incorrecta")
+                row[4] = int(row[4]) + 1
+                entradas_validas = True
+                writer.writerow([row[0],row[1],row[2],row[3],row[4]])
+    
+    if not entradas_validas:
+        messagebox.showerror("Error", "Verifique los datos")
+
+
+
+def validar_ingreso(ventana_ingreso, id_usuario_ingreso, clave_ingreso):
+    ingreso_exitoso = False
+
+    with open('./archivos csv/usuarios.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row and row[0] == id_usuario_ingreso and row[1] == clave_ingreso and int(row[4]) <= 3:
+                ingreso_exitoso = True
                 messagebox.showinfo("Éxito", "Ingreso exitoso")
                 ventana_ingreso.destroy()  # Cerrar la ventana de ingreso
                 mostrar_ventana_principal()  # Abrir la ventana principal
-                return
+            elif row and row[0] == id_usuario_ingreso and int(row[4]) > 3:
+                ingreso_exitoso = True
+                messagebox.showerror("Error", "Usuario bloqueado")
 
-    messagebox.showerror("Error", "Ingreso fallido. Verifica tus credenciales.")
+    if not ingreso_exitoso:
+        messagebox.showerror("Error", "Ingreso fallido. Verifica tus credenciales.")
 
-    with open('usuarios.csv', 'r') as file:
-
-        reader = csv.reader(file)
-
-        for row in reader:
-            if row and row[0] == id_usuario_ingreso and row[1] == clave_ingreso:
-                messagebox.showinfo("Éxito", "Ingreso exitoso")
-                ventana_ingreso.destroy()  # Cerrar la ventana de ingreso
-                mostrar_ventana_principal()  # Abrir la ventana principal
-                return
-
-    messagebox.showerror("Error", "Ingreso fallido. Verifica tus credenciales.")
 
 def mostrar_ventana_principal():
     """
@@ -219,9 +220,9 @@ def mostrar_ventana_principal():
         messagebox.showinfo("Resultado", f"Resultado: {resultado}")
 
     # Configuraciones iniciales de la ventana
-    ventana_principal = Toplevel()
+    ventana_principal = Tk()
     ventana_principal.title("TP Grupal Parte 1 - Grupo: [Codeo]")
-    ventana_principal.iconbitmap("logo.ico")
+    ventana_principal.iconbitmap("./logos/logo.ico")
     ventana_principal.resizable(0, 0)
     ventana_principal.geometry("350x250")
     ventana_principal.config(bg="#00FA9A")
